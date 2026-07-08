0c866eaf587077c326ba96c43d96ae7bdca89835
Muhammad Hassan <malikmohammadhassan8@gmail.com>
2026-07-08 12:30:48 +0500

    Update GradeCalculator2.java

--- GradeCalculator2.java
+++ GradeCalculator2.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class GradeCalculator {
+public class GradeCalculator2 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
